386546ed57d2c426af3a4c24c9e90778219a9c62
md-roqunuzzaman <khanrokon571@gmail.com>
2026-01-05 21:56:43 +0800

    some amination added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+        keyframes: {
+          fadeUp: {
+            '0%': { opacity: '0', transform: 'translateY(40px)' },
+            '100%': { opacity: '1', transform: 'translateY(0)' },
+          },
+          float: {
+            '0%, 100%': { transform: 'translateY(0)' },
+            '50%': { transform: 'translateY(-15px)' },
+          },
+        },
+        animation: {
+          fadeUp: 'fadeUp 1s ease-out forwards',
+          float: 'float 3s ease-in-out infinite',
+        },
+    },
   },
   plugins: [],
 }
